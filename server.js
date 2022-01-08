210bb7788757482e4e60c7372ba0d500b5274ef4
nolim1t <hello@nolim1t.co>
2022-01-08 19:37:49 +0700

    Remove kruft. add uuid support

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const port = 3000
 
 // for envirobment
 const process = require("process");
+// for uuid
+const { v4: uuidv4 } = require('uuid');
 
 // create PG Client
 /*
@@ -26,21 +28,6 @@ const client = new Client({
 const app = new Koa();
 */
 
-
-/*
-app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
-
-app.use(bodyParser({
-    multipart: true,
-    urlencoded: true
-}));
-*/
-
 // express body parser
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -50,8 +37,6 @@ var results = {
   "meta": {"status": 400, "msg": "Not implemented"}
 };
 app.use(function (req, res, next) {
-  console.log('Time:', Date.now());
-
   // reset results
   results.meta.status = 400;
   results.meta.msg = "Bad request";
@@ -60,6 +45,7 @@ app.use(function (req, res, next) {
   results.meta.debug = undefined;
   next()
 });
+
 //  Default endpoint
 app.get('/', (req, res) => {
   results.meta.status = 200;
@@ -126,7 +112,7 @@ app.get('/1/list', (req, res) => {
       // stubbed places
       var places = [
         {
-            device_id: "mostlikely-a-uuid",
+            device_id: uuidv4(),
             device_location: "cafe",
             place: {
                 name: "Yellow Coworking Space",
@@ -167,88 +153,3 @@ app.get('/1/list', (req, res) => {
 app.listen(port, () => {
   console.log(`Developpa app listening at http://localhost:${port}`)
 })
-// Koa stuff
-/*
-function base(ctx, next) {
-    if ('/' == ctx.path) {
-        console.log("default / path detected");
-        if ('POST' == ctx.request.method) {
-            console.log('POST detected');
-            console.log("request: " + JSON.stringify(ctx.request.body));
-        } else {
-            console.log('GET detected')
-            console.log("request: " + JSON.stringify(ctx.request.query));
-        }
-        ctx.body = 'Hello World';
-    } else if ('/1/verify' == ctx.path) {
-        if ('POST' == ctx.request.method) {
-            var verify_status;
-            var verify_message;
-            if (ctx.request.body.fakeVerify !== undefined) {
-                verify_status = JSON.parse(ctx.request.body.fakeVerify.toLowerCase());
-                if (verify_status == false) {
-                    verify_message = "Could not verify device";
-                }
-            } else {
-                verify_status = true;
-                verify_message = "Verified successfully";
-            }
-            if (ctx.request.body.device === undefined) {
-                verify_status = false;
-                verify_message = "Must specify a valid 'device' parameter";
-            }
-            results = {
-                verified: verify_status,
-                verify_msg: verify_message,
-                meta: {"status": "OK", "msg": "Success", "timestamp": new Date().getTime()}
-            };
-            ctx.body = JSON.stringify(results);
-        } else {
-            results = {
-                meta: {"status": "Method Not supported", "msg": "Error", "timestamp": new Date().getTime()}
-            };
-            ctx.body = JSON.stringify(results);
-        }
-    } else if ('/1/list' == ctx.path) {
-        results = {
-            result: [
-                {
-                    device_id: "mostlikely-a-uuid",
-                    device_location: "cafe",
-                    place: {
-                        name: "Yellow Coworking Space",
-                        coords: [18.79829143251964, 98.96882473444388],
-                        address: "16 2 Nimmanahaeminda Road, Su Thep, Mueang Chiang Mai, Chiang Mai 50200",
-                        meta: {
-                            "bounty": 5000,
-                            "purifiers": 8,
-                            "floors": 2,
-                            "rating": 1,
-                        }
-                    },
-                    aqi: {
-                        now: 9,
-                        week: 13,
-                        month: 10
-                    },
-                }
-            ],
-            meta: {"status": "OK", "msg": "Processed", "timestamp": new Date().getTime()}
-        };
-        if ('GET' == ctx.request.method) {
-            ctx.body = JSON.stringify(results);
-        } else {
-            results = {
-                meta: {"status": "Not supported", "msg": "Error", "timestamp": new Date().getTime()}
-            };
-            ctx.body = JSON.stringify(results);
-        }
-    } else {
-        console.log("Default path");
-        next();
-    }
-}
-
-app.use(compose([base]));
-app.listen(3000);
-*/
